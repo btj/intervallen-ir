e349e0250f556fcc6885ca70ec1f81f8b1710400
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2026-02-17 11:14:51 +0100

    Getters en setters

---
+++ intervallen-ir/src/intervallen/IntervallenTest.java
@@ -0,0 +1,35 @@
+package intervallen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntervallenTest {
+	
+	class Interval {
+		int ondergrens;
+		int bovengrens;
+	}
+	
+	int getOndergrens(Interval interval) { return interval.ondergrens; }
+	
+	int getBovengrens(Interval interval) { return interval.bovengrens; }
+	
+	void setOndergrens(Interval interval, int ondergrens) {
+		interval.ondergrens = ondergrens;
+	}
+	
+	void setBovengrens(Interval interval, int bovengrens) {
+		interval.bovengrens = bovengrens;
+	}
+
+	@Test
+	void test() {
+		Interval myInterval = new Interval();
+		setOndergrens(myInterval, 3);
+		setBovengrens(myInterval, 7);
+		int lengte = getBovengrens(myInterval) - getOndergrens(myInterval);
+		assertEquals(4, lengte);
+	}
+
+}
