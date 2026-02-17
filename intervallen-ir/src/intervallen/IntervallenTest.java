d67a5c96da00492f7dc23ec56cfc2bade9d24204
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2026-02-17 12:19:03 +0100

    Test case aangevuld

--- intervallen-ir/src/intervallen/IntervallenTest.java
+++ intervallen-ir/src/intervallen/IntervallenTest.java
@@ -12,6 +12,16 @@ class IntervallenTest {
 		Interval myInterval = new Interval(3, 7);
 		int lengte = myInterval.getBovengrens() - myInterval.getOndergrens();
 		assertEquals(4, lengte);
+		
+		myInterval.setOndergrens(2);
+		assertEquals(2, myInterval.getOndergrens());
+		assertEquals(6, myInterval.getBovengrens());
+		assertEquals(4, myInterval.getLengte());
+		
+		myInterval.setBovengrens(8);
+		assertEquals(2, myInterval.getOndergrens());
+		assertEquals(8, myInterval.getBovengrens());
+		assertEquals(6, myInterval.getLengte());
 	}
 
 }
